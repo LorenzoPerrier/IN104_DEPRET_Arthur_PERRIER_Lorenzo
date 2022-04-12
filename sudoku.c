de8e992ad432efd0bbe0eeff33762558393a8a9e
arthur1911 <arthurdepret@gmail.com>
2022-04-12 15:01:13 +0200

    Modif Sudoku Arthur

--- sudoku.c
+++ sudoku.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+//#define	int matrix[9][9];
 
-#define int matrice[9][9];
+void fillGrid(int tab[3][3]) {
+	int Numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // Tableau des valeurs possibles
+	int random;
 
-
-bool check_num(int *tab, int i, int num)
-{
-	for (int k = 0; k<i;i++)
-	{
-		if tab[i]==num {return true;}
-	}
-return false;
-}
-
-int ** create_matrice_diag()
-{
-	int matrice3[9];
-
-	matrice3[0] = rand()%9 +1;
-
-	for (int i = 1; i<9;i++)
-	{ 
-		do {int num = rand()%9 +1;}
-		while {check_num(matrice3,i,num)}
-		matrice3[i]=num;		
+	for (int i=0; i<3; i++) {
+		for (int j=0; j<3; j++) {
+			random = rand()%9;
+			while (Numbers[random] == 0) {
+				random = rand()%9;
+				}
+			tab[i][j] = Numbers[random];
+			Numbers[random] = 0;
+			}
 		}
 	}
-
 	
 
+void display(int tab[3][3]) {
+	for (int i=0; i<3; i++) {
+		for (int j=0; j<3; j++) {
+			printf("%d", tab[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+int main() {
+	srand(time(0));
+	int up_right[3][3];
+	int up_left[3][3];
+	int down_right[3][3];
+	int down_left[3][3];
+	fillGrid(up_left);
+	fillGrid(up_right);
+	fillGrid(down_right);
+	fillGrid(down_left);
+	display(matrix);
 }
